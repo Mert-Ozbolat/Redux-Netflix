089fe86383ee8a24c2b48e118276103eb934eeae
Mert-Ozbolat <mertozbolat008@gmail.com>
2024-11-30 21:10:10 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Detail from './pages/detail'
+import Favourites from './pages/favourites'
+import Home from './pages/home'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movie/:id' element={<Detail />} />
+        <Route path='/favourites' element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
